902daa60922ab9c0c9d9926c5cdb08f48b21940d
Isaac <zyskson@gmail.com>
2023-08-27 20:49:02 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import CoverWrapper from '#/components/cover-wrapper'
 import React from 'react'
 
-type Props = {}
-
-const Page = (props: Props) => {
+import Me from '#/public/static/images/me.jpg';
+const Page = ({ children, ...props}:any) => {
   return (
-    <div>Page</div>
+    <div className="page-scroller">
+        <CoverWrapper img={Me}/>
+    <div className='container mx-auto h-screen max-w-4xl py-6 lg:py-10 bg-item_pink'>
+
+    </div>
+    </div>
   )
 }
 
---
+++ components/cover-wrapper.tsx
@@ -0,0 +1,42 @@
+import Image, { StaticImageData } from 'next/image';
+import React from 'react';
+
+type Props = {
+  img: {
+    base64: string;
+    img: {
+      src: string;
+      height: number;
+      width: number;
+    };
+    blurDataURL?: string;
+  }| StaticImageData;
+};
+
+const CoverWrapper = ({ img }: Props) => {
+  return (
+    <div className="page-cover-wrapper">
+      {'base64' in img ? (
+        <Image
+          priority={true}
+          className="page-cover"
+          src={img.img.src}
+          alt="page cover wrapper"
+          height={img.img.height}
+          width={img.img.width}
+          placeholder="blur"
+          blurDataURL={img.base64}
+        />
+      ) : (
+        <Image
+          priority={true}
+          className="page-cover"
+          src={img}
+          alt="page cover wrapper"
+        />
+      )}
+    </div>
+  );
+};
+
+export default CoverWrapper;
